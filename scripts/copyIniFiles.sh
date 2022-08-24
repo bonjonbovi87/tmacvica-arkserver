2a6b805060a120c6d87ca5bb028b29b8d3d19ba3
bonjonbovi87 <tmacvicar@gmail.com>
2022-08-23 20:04:35 -0400

    local updates

--- scripts/copyIniFiles.sh
+++ scripts/copyIniFiles.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+git commit -a -m "local updates"
+git pull
+git push
+
 cp /home/arkserver/tmacvica-arkserver/config/Game*.ini /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer
 
