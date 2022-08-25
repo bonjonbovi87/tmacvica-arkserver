b23302f6e10c5a605c07f27de53ab97b6f414e13
bonjonbovi87 <tmacvicar@gmail.com>
2022-08-25 14:45:13 -0400

    local updates

--- scripts/copyIniFiles.sh
+++ scripts/copyIniFiles.sh
@@ -6,12 +6,8 @@ git push
 
 cp /home/arkserver/tmacvica-arkserver/config/Game*.ini /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer
 
-#sed "s/serveradminpassword/$ARKSERVER_ADMIN_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
-#sed "s/serverpassword/$ARKSERVER_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
-
 sed "s/serveradminpassword/$ARKSERVER_ADMIN_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
-sed "s/serverpassword//g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
-
+sed "s/serverpassword/$ARKSERVER_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 
 cp /home/arkserver/tmacvica-arkserver/config/PlayersExclusiveJoinList.txt /home/arkserver/serverfiles/ShooterGame/Binaries/Linux/
 
