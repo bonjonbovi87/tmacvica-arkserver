575b445cd322e01e0c18644c9ac021a100432cb3
bonjonbovi87 <tmacvicar@gmail.com>
2022-08-26 13:30:15 -0400

    local updates

--- scripts/copyIniFiles.sh
+++ scripts/copyIniFiles.sh
@@ -6,6 +6,7 @@ git push
 
 cp /home/arkserver/tmacvica-arkserver/config/Game*.ini /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer
 
+source /etc/environment
 sed "s/serveradminpassword/$ARKSERVER_ADMIN_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 sed "s/serverpassword/$ARKSERVER_PASSWORD/g" -i /home/arkserver/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 
